2adaa4349a3b8367375d324618588412acd457f0
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-05-15 16:15:29 +0300

    Create edit.php

---
+++ views/edit.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <h1>Редактировать запись</h1>
+    <form>
+        <div class="form-group">
+            <label for="todoName">Имя</label>
+            <input type="name" class="form-control" id="todoName" readonly>
+        </div>
+        <div class="form-group">
+            <label for="todoEmail">E-mail</label>
+            <input type="password" class="form-control" id="todoEmail"  aria-describedby="emailHelp" readonly>
+        </div>
+        <div class="form-group">
+            <label for="todoText">Текст</label>
+            <textarea class="form-control" id="todoText" rows="3"></textarea>
+        </div>
+            <button type="submit" class="btn btn-primary">Редактировать</button>
+    </form>
+</div>
