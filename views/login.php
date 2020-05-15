b604f2b576711324f6008a1d942a23e6c490d982
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-05-15 16:15:43 +0300

    Create login.php

---
+++ views/login.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h1>Авторизация</h1>
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Логин</label>
+            <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите логин">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Пароль</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Пароль">
+        </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
