4e328e7d8aef4579ace107cb64d980b75074b0f6
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-05-15 16:06:03 +0300

    Create _db.sql

---
+++ config/_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `phptest`.`todo` ( 
+    `id` TINYINT(11) NOT NULL AUTO_INCREMENT , 
+    `name` VARCHAR(255) NOT NULL , 
+    `email` VARCHAR(255) NOT NULL , `text` TEXT NOT NULL , PRIMARY KEY (`id`)) 
+ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
