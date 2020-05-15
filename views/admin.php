269e5179dba2611ac38ba926d89bcd0dfeb1a844
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-05-15 16:15:14 +0300

    Create admin.php

---
+++ views/admin.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <h1>Все записи</h1>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Заголовок</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Выполнено</label>
+            </div>
+            <a href="#" class="btn btn-primary">Редактировать</a>
+            <a href="#" class="btn btn-primary">Удалить</a>
+        </div>
+    </div>
+</div>
