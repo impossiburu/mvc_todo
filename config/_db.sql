acc73cf921b28a756fd9fa5485ef4074d5333bd0
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-05-15 16:13:52 +0300

    Update _db.sql

--- config/_db.sql
+++ config/_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `phptest`.`todo` ( 
+CREATE TABLE `your_db_name`.`todo` ( 
     `id` TINYINT(11) NOT NULL AUTO_INCREMENT , 
     `name` VARCHAR(255) NOT NULL , 
     `email` VARCHAR(255) NOT NULL , `text` TEXT NOT NULL , PRIMARY KEY (`id`)) 
