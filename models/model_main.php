bbb08a12fafce0c52318e7968fb3923cd5ed1d9d
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-06-04 03:44:59 +0300

    Update model_main.php

--- models/model_main.php
+++ models/model_main.php
@@ -2,13 +2,14 @@
 
 class Model_Main extends Model {
     
-    public function getData() {
+    public function getData($id = null) {
         $data = db::query('SELECT * FROM todo');
         return $data;
     }
 
     public function insertData(array $insertData) {
         $insert = db::query('INSERT INTO `todo`(`name`, `email`, `text`) VALUES (?, ?, ?)', array_values($insertData));
+        //this may give an error on hosting 
         if ($insert) {
             echo '<b>Success</b>';
             header("location: action");
