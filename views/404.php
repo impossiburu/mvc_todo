38d83fef78c8ff789be7840e55e29d401564c87c
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-06-04 03:51:54 +0300

    Create 404.php

---
+++ views/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>404</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="login.css">
+</head>
+<body >
+<div class="container">
+    <h1>Упс, что-то пошло не так</h1>
+    <a href="/main/action">Вернуться на главную</a>
+</div>
+</body>
+</html>
