b5c6a7435db62b337e0e74af1c04cf4a6df4cc49
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-06-04 03:31:36 +0300

    Update _db.sql

--- config/_db.sql
+++ config/_db.sql
@@ -1,5 +1,14 @@
-CREATE TABLE `your_db_name`.`todo` ( 
+CREATE TABLE `todo` ( 
     `id` TINYINT(11) NOT NULL AUTO_INCREMENT , 
     `name` VARCHAR(255) NOT NULL , 
-    `email` VARCHAR(255) NOT NULL , `text` TEXT NOT NULL , PRIMARY KEY (`id`)) 
+    `email` VARCHAR(255) NOT NULL , 
+    `text` TEXT NOT NULL , 
+    PRIMARY KEY (`id`)) 
+ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `users` ( 
+    `id` TINYINT(11) NOT NULL AUTO_INCREMENT , 
+    `user` VARCHAR(255) NOT NULL , 
+    `password` VARCHAR(255) NOT NULL ,
+     PRIMARY KEY (`id`)) 
 ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
