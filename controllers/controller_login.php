1ef702438b9ef9194fc3a784a162685bb62d9b9d
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-06-04 03:37:09 +0300

    Update controller_login.php

--- controllers/controller_login.php
+++ controllers/controller_login.php
@@ -2,8 +2,30 @@
 
 class Controller_Login extends Controller {
 
-    public function action()
-    {
+    function __construct() {
+        $this->model = new Model_Login();
+        $this->view = new View();
+    }
+    
+    public function action() {
+        $usersData = $this->model->getData();
+        $user = [];
+        foreach ($usersData as $userData) {
+            $user = $userData;
+        }
+        
+        if (isset($_POST['login']) && isset($_POST['password'])) {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
+
+            if ($login == $user['user'] && $password == $user['password']) {
+                session_start();
+                $_SESSION['admin'] = $password;
+                header('Location: /php-test/admin/action');
+            } else {
+                header('Location: /php-test/login/action');
+            }
+        }
         $this->view->render('login.php', 'template.php');
     }
 }
