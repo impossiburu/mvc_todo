82c819bfcd8529a05fe1c4225b7f17f889fe2696
impossiburu <54071319+impossiburu@users.noreply.github.com>
2020-06-04 03:48:27 +0300

    Update edit.php

--- views/edit.php
+++ views/edit.php
@@ -1,18 +1,18 @@
 <div class="container">
-    <h1>Редактировать запись</h1>
-    <form>
-        <div class="form-group">
-            <label for="todoName">Имя</label>
-            <input type="name" class="form-control" id="todoName" readonly>
-        </div>
-        <div class="form-group">
-            <label for="todoEmail">E-mail</label>
-            <input type="password" class="form-control" id="todoEmail"  aria-describedby="emailHelp" readonly>
-        </div>
+    <h1>Редактировать запись</h1>   
+    <form action="/edit/update" method="POST">
+    <input type="hidden" name="id" size="80" value="<?php echo $data['id'] ?>">
         <div class="form-group">
             <label for="todoText">Текст</label>
-            <textarea class="form-control" id="todoText" rows="3"></textarea>
+            <textarea class="form-control" id="todoText" name="text" rows="3"><?php echo $data['text'] ?></textarea>
         </div>
-            <button type="submit" class="btn btn-primary">Редактировать</button>
+        <?php if ($data['done']) {
+            $isDone = 'checked';
+        } ?>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" name="done" id="checkBox" <?php echo $isDone ?>>
+            <label class="form-check-label" for="checkBox">Выполнено</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Редактировать</button>
     </form>
 </div>
